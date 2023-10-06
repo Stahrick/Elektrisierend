b413d7e77728471ac32804fbc01bde84315b9da4
Patrick Stalheuer <Stahrick@users.noreply.github.com>
2023-10-06 19:07:38 +0200

    New features

--- StromZaehler.py
+++ StromZaehler.py
@@ -1,14 +1,18 @@
+import os
 import random
 import math
+import logging
+import sys
 
 from datetime import datetime
 from flask import Flask, request, make_response
 
 app = Flask(__name__)
+logging.basicConfig("./log.txt", encoding="UTF-8", format='%(asctime)s %(levelname)s:%(message)s',filemode="w", level=logging.INFO)
 
-meter = 0
-last_request = 0
-maintenance_activation_time = datetime.fromisocalendar(1000,1,1)
+meter = None
+last_request = None
+maintenance_activation_time = None
 
 average_kwh_per_sec = 2050 / 365 / 24 / 60 / 60  # Average 2050kwh per person per year
 
@@ -16,6 +20,7 @@ average_kwh_per_sec = 2050 / 365 / 24 / 60 / 60  # Average 2050kwh per person pe
 @app.route("/meter", methods=["GET"])
 def get_meter():
     update_meter()
+    logging.info(f"GET meter from host {request.host}")
     return {"Meter": round(meter, 4)}
 
 
@@ -26,13 +31,22 @@ def set_meter():
     global meter
 
     cur_time = datetime.now()
-    if ((cur_time-maintenance_activation_time).total_seconds()/60) > 5:
+    # Check if maintenance mode was activated more than 5 minutes ago
+    if maintenance_activation_time is None or ((cur_time-maintenance_activation_time).total_seconds()/60) > 5:
+        logging.warning(f"Try to SET meter from host {request.host}, but failed caused by deactivated maintenance mode")
         return make_response("Maintenance disabled", 200)
 
     msg = request.json
     meter = msg["Meter"]
     last_request = datetime.now()
+    logging.info(f"SET meter from host {request.host} to {meter}")
     return make_response("Meter updated to " + str(meter), 200)
+
+
+@app.route("/restart", methods=["GET"])
+def restart():
+    # Restart device
+    return make_response("Restart triggered. Wait at least 30 sec before reconnecting.", 200)
 
 
 @app.route("/maintenance", methods=["POST"])
@@ -45,11 +59,18 @@ def activate_maintenance():
         return make_response("Invalid JSON body", 500)
     if msg["Active"]:
         maintenance_activation_time = datetime.now()
+        logging.info(f"Activated maintenance from host {request.host}")
         return make_response("Maintenance activated", 200)
     else:
         maintenance_activation_time = datetime.fromisocalendar(1000,1,1)
+        logging.info(f"Deactivated maintenance from host {request.host}")
         return make_response("Maintenance deactivated", 200)
 
+
+#@app.route("/certrenew", methods=["POST"]):
+def renew_cert():
+    # Renew the company certificate
+    return make_response("Certificate renewed", 200)
 
 def update_meter():
     global meter
@@ -57,9 +78,11 @@ def update_meter():
     passed_sec = (cur_time-last_request).total_seconds()
     amount_added = random.uniform(average_kwh_per_sec-float("1e-5"), average_kwh_per_sec+float("1e-5"))
     meter += passed_sec * amount_added
+    logging.info(f"Updated meter to {meter}")
 
 
 def start_server():
+    logging.info("Start webserver")
     app.run(host="0.0.0.0", debug=True, port=80)
 
 
@@ -68,7 +91,13 @@ def init():
     global last_request
     meter = random.randrange(0, 50)
     last_request = datetime.now()
+    logging.info(f"Initialized meter with {meter} KWH and last_request {last_request}")
 
+
+def factory_reset():
+    # Triggers the factory reset
+    logging.warning("Factory reset triggered")
+    init()
 
 init()
 start_server()
