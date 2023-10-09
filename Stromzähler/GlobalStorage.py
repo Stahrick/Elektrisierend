4e03e2495cf49bcbf12a97fc93ce2ce127dc9626
Patrick Stalheuer <Stahrick@users.noreply.github.com>
2023-10-09 20:31:33 +0200

    Add export and import functions for future implementation of data persistence over runtimes

--- Stromzähler/GlobalStorage.py
+++ Stromzähler/GlobalStorage.py
@@ -1,3 +1,5 @@
+import json
+
 meter_dict = {}
 
 
@@ -10,8 +12,19 @@ def remove_meter(uuid):
 
 
 def list_meters():
+    global meter_dict
     return list(meter_dict.keys())
 
 
 def get_meter(uuid):
     return meter_dict[uuid] if uuid in meter_dict else None
+
+
+def export_meters():
+    global meter_dict
+    return meter_dict
+
+
+def import_meters(import_data):
+    global meter_dict
+    meter_dict = import_data
