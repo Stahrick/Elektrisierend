74f2f07065a7bd0ad8802984a80030a788cfa68e
Patrick Stalheuer <Stahrick@users.noreply.github.com>
2023-11-03 19:54:37 +0100

    Add some more logging when clearance mismatches happen

--- Stromzähler/security/Decorator.py
+++ Stromzähler/security/Decorator.py
@@ -1,4 +1,5 @@
 import enum
+import logging
 from functools import wraps
 
 import jwt
@@ -10,6 +11,8 @@ from GlobalStorage import list_meters, get_meter
 
 import re
 
+
+logging.getLogger(__name__)
 class ClearanceLevel(enum.Enum):
     LOCAL = "Local"
     LOW = "Low"
@@ -43,13 +46,16 @@ def clearance_level_required(level):
                     kwargs["user_id"] = cookie["user_id"]
                     return func(*args, **kwargs)
                 except jwt.InvalidTokenError as e:
+                    logging.warning(f"Invalid token from {request.host}")
                     return "Invalid token", 401
             elif level == ClearanceLevel.LOCAL:
                 global local_ip_pattern
                 if local_ip_pattern.match(request.host):
                     return func(*args, **kwargs)
                 else:
+                    logging.warning(f"Access from extern to local func from {request.host}")
                     return "Can only be accessed on the local network", 401
+            logging.warning(f"No sec clearance for level [{level}] from {request.host}")
             return "No security clearance for this action", 401  # Unauthorized
         return wrapper
     return decorator
