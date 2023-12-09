a3941319f449e16b747512502958146c4bda55eb
kimi no na wa <qwerty@qwerty.com>
2023-12-10 00:11:33 +0100

    changed own url to https

--- webportal_msb/config.py
+++ webportal_msb/config.py
@@ -1,5 +1,5 @@
 meter_url = 'https://localhost:25565'
 kp_url = 'https://localhost:4000'
-own_url = 'http://localhost:5000'
+own_url = 'https://localhost:5000'
 mycert = ('localhost.crt', 'localhost.key')
 root_ca = 'RootCA.crt'
